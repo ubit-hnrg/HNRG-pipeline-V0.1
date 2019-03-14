23174f6a8b3265f112c402bb0e697473118ea625
ubit-hnrg <arieljberenstein@gmail.com>
2019-03-14 14:10:16 -0300

    add htslib deprndency

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ fi
 
 ## prerequisite for samtools and htslib.
 sudo apt-get install zlib1g-dev
-
+sudo apt-get install libbz2-dev
 
 ################################################
 ######   local installations (tools).   ########
